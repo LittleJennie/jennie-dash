24266f057ce0274cb60ea35e280d7a3642220113
Jennie Zeng <jenniezeng1@gmail.com>
2019-12-05 22:34:40 -0800

    fetch yelp businesses, hook up front and back

--- client/src/helperfunctions.js
+++ client/src/helperfunctions.js
@@ -13,7 +13,21 @@ const helpers = {
     } catch(err) {
       return err;
     }
-  }
+  }, 
+
+  fetchYelpBusinesses: async (input) => {
+    const result = await axios.get('/order/yelpBusinessSearch', 
+      {
+        params: { input },
+      }
+    );
+
+    try {
+      return result.data;
+    } catch(err) {
+      return err;
+    }
+  },
 };
 
 export default helpers;
